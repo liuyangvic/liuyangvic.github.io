4603a836e4292fb85bf9b8edb7966902408e56c6
Yang Liu <liuyangvic@msn.com>
2020-04-24 02:45:16 +0900

    Update read_excel.py

--- study python/practise/2p/read_excel.py
+++ study python/practise/2p/read_excel.py
@@ -1,3 +1,20 @@
+"""
+Sample禁則Lib(Copy ColorMode禁則)
+===
+禁則pattern：
+
+to_be_set_cl_value
+is limited(制約され) by
+limited_by_cl_value,limited_by_cl_value2, limited_by_cl_value3 etc.
+
+it will fianlly be correctted（修正され） to
+correct_to_cl_value.
+
+after it is corrected,
+affected_cl_value1, affected_cl_value2, affected_cl_value3 etc.
+will also be changed（影響され）
+
+"""
 import xlrd
 
 file = 'P2Pカラーモード(自動生成用).xlsx'
@@ -21,41 +38,100 @@ def read_auto_generate_sheet(wb):
         sheet1 = wb.sheet_by_name(sheet_name) # specify sheet by sheet name
         print("=============Sheet %s============="%sheet_name)
         begin_row = 4 #
-        end_row = 13 #
+        end_row = 7 #
         begin_column = 0 #
         end_column = 3 #
         ###　variable parameters when sheet modified ###　
                 
         print("Reading sheet:",sheet1.name, "has" ,sheet1.nrows, "rows" ,sheet1.ncols, "colums" )
 
-        for j in range(begin_column,end_column):
-                for i in range(begin_row,end_row):
-                        auth_value = sheet1.cell(i,begin_column-1).value
-                        account_value = sheet1.cell(begin_row-1,j).value
-                        limit_value = sheet1.cell(i,j).value
-                        print(auth_value,",",account_value,",",limit_value)
+        for i in range(begin_row,end_row+1):
+                # old product cl value
+                to_be_set_cl_value = sheet1.cell(i,begin_column).value
+                # can be limited by cl1 cl2 cl3 etc.
+                limited_by_cl_value = sheet1.cell(i,begin_column+1).value
+                # finally it will be correct to following value
+                correct_to_cl_value = sheet1.cell(i,begin_column+2).value
+                # it will also also
+                affected_cl_value = sheet1.cell(i,begin_column+3).value
+                print(str(to_be_set_cl_value).ljust(10),",",str(limited_by_cl_value).ljust(10),",",str(correct_to_cl_value).ljust(10),",",str(affected_cl_value).ljust(10)) 
 
-                        # transform limit value        
-                        if limit_value == '◎':
-                                limit_value = '2'
-                        elif limit_value == '○':
-                                limit_value = '1'
-                        elif limit_value =='×':
-                                limit_value = '0'
-                        else:
-                                print('illegal limit value has read:'+limit_value)
-                                limit_value = ''
+                copy_color_mode.append([to_be_set_cl_value,limited_by_cl_value,correct_to_cl_value,affected_cl_value])
 
-                        copy_color_mode.append([auth_value,account_value,limit_value])                        
-                        
+def trans_to_be_set_cl_value(in_value,out_value):
+
+                # transform spec(仕様) to num(実装)      
+                if in_value == '◎':
+                        out_value = '2'
+                elif in_value == '○':
+                        limit_value = '1'
+                elif in_value =='×':
+                        out_value = '0'
+                else:
+                        print('illegal in_value has read:'+in_value)
+                        in_value = ''
+
+                        return -1
+
+                return out_value
+        
+def trans_to_limited_by_cl_value(in_value,out_value):
+
+                # transform spec(仕様) to num(実装)      
+                if in_value == '◎':
+                        out_value = '2'
+                elif in_value == '○':
+                        limit_value = '1'
+                elif in_value =='×':
+                        out_value = '0'
+                else:
+                        print('illegal in_value has read:'+in_value)
+                        in_value = ''
+
+                        return -1
+
+                return out_value
+        
+def trans_to_affected_cl_value(in_value,out_value):
+
+                # transform spec(仕様) to num(実装)      
+                if in_value == '◎':
+                        out_value = '2'
+                elif in_value == '○':
+                        limit_value = '1'
+                elif in_value =='×':
+                        out_value = '0'
+                else:
+                        print('illegal in_value has read:'+in_value)
+                        in_value = ''
+
+                        return -1
+
+                return out_value
+        
+def trans_to_affected_cl_value(in_value,out_value):
+
+                # transform spec(仕様) to num(実装)      
+                if in_value == '◎':
+                        out_value = '2'
+                elif in_value == '○':
+                        limit_value = '1'
+                elif in_value =='×':
+                        out_value = '0'
+                else:
+                        print('illegal in_value has read:'+in_value)
+                        in_value = ''
+
+                        return -1
+
+                return out_value
+
                         
 def main():
         
         wb = read_excel()
 
         if wb != -1:
-                read_fx_sheet(wb)
-                read_ibg_sheet(wb)
-                read_mn_sheet(wb)
+                read_auto_generate_sheet(wb)
 
 main()
