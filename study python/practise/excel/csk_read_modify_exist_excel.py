e6110ed814d21c8c74d8e5ddfa0e0995d29b08ac
Yang Liu <liuyangvic@msn.com>
2021-03-19 17:04:26 +0900

    Update csk_read_modify_exist_excel.py

--- study python/practise/excel/csk_read_modify_exist_excel.py
+++ study python/practise/excel/csk_read_modify_exist_excel.py
@@ -11,6 +11,7 @@ import openpyxl # modify exist excel lib
 #read excel info
 read_file = '01_イニシアティブ_本部内業界活動.xlsx'
 read_sheet1_name1 = "タスク・運営委員リスト_202011"
+max_rows = 60
 
 #write excel info
 write_file = 'pm_project_jp_listOK.xlsx'
@@ -48,34 +49,98 @@ def open_write_excel():
                 
                 """
                 for cell in list(write_sheet.rows)[0]:
-                    if(cell.value == "承認希望期限"):
+                    if cell.value == "承認希望期限" :
                         cell.value =  "承認希望期限_modified"
                     print(cell.value)
                 """
                 """
                 for i in range(begin_row,write_sheet.nrows):
                         for j in range(begin_column,write_sheet.ncols):
-                                if( sheet1.cell(i,j).value != ""):
+                                if sheet1.cell(i,j).value != "":
                                     sample_data.append([sheet1.cell(0,j).value,sheet1.cell(i,j).value])
                                     print([sheet1.cell(0,j).value,sheet1.cell(i,j).value])                
                 """
                 begin_column = 1
                 begin_row = 2
-                cnt = 1
                 
+                
+                cnt = 1
                 for j in range(begin_column,158):
-                    if ( write_sheet.cell(1,j).value == "終了実績日時" ):
-                        for i in sample_data:
-                            print(i[0])
-                            
-                            if(i.pop([0])　== "終了日"):
+                    if  (write_sheet.cell(1,j).value == "終了実績日時") | (write_sheet.cell(1,j).value == "終了予定日時") :
+                        for i in sample_data:                            
+                            if i[0]== "終了日" :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1
+                print("終了実績日時 write completed!")
+                
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  (write_sheet.cell(1,j).value == "開始実績日時") | (write_sheet.cell(1,j).value == "開始予定") :
+                        for i in sample_data:                            
+                            if i[0]== "開始日" :
                                 print(i[0],",",i[1])
-                                print("**********************************:")
-                            """
-                                #write_sheet.cell(cnt,j).value = i[1]
-                                #cnt++
-                                #print(write_sheet.cell(i,j).value)
-                            """
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1
+                print("開始実績日時 write completed!")
+                
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "プロジェクトメンバ" :
+                        for i in sample_data:                            
+                            if i[0]== "担当者" :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "管理レベル" :
+                        for i in sample_data: 
+                            if cnt < max_rows :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = "部署"
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "ステータス" :
+                        for i in sample_data:
+                            if cnt < max_rows :                            
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = "OPEN"
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "プロジェクト名" :
+                        for i in sample_data:                            
+                            if i[0]== "委員・イニシアティブ" :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "部門名" :
+                        for i in sample_data:                            
+                            if i[0]== "部署" :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "本部・ユニット名" :
+                        for i in sample_data:                            
+                            if cnt < max_rows :
+                                #print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = "臨床開発本部"
+                                cnt=cnt+1
+                cnt = 1
+                for j in range(begin_column,158):
+                    if  write_sheet.cell(1,j).value == "説明" :
+                        for i in sample_data:                            
+                            if i[0]== "業界団体URL" :
+                                print(i[0],",",i[1])
+                                write_sheet.cell(cnt+1,j).value = i[1]
+                                cnt=cnt+1               
+                
                 
                 """
                 print("before:" ,write_sheet.cell(column=0,row=0).value)
@@ -85,6 +150,7 @@ def open_write_excel():
                 """
                 #print("This excel includes following sheets:",write_excel.sheet_names()) #print including sheets name
                 write_excel.save(filename=write_file)
+                print("all write completed!")
                 return 0
 
         except Exception as e:
@@ -111,12 +177,13 @@ def read_1_sheet(wb):
         ###　variable parameters when sheet modified ###　
                 
         print("Reading sheet:",sheet1.name, "has" ,sheet1.nrows, "rows" ,sheet1.ncols, "colums" )
-           
+        max_rows = sheet1.nrows
+        
         for i in range(begin_row,sheet1.nrows):
                 for j in range(begin_column,sheet1.ncols):
-                        if( sheet1.cell(i,j).value != ""):
-                            sample_data.append([sheet1.cell(0,j).value,sheet1.cell(i,j).value])
-                            print([sheet1.cell(0,j).value,sheet1.cell(i,j).value])
+                        #if sheet1.cell(i,j).value != "":
+                            sample_data.append([sheet1.cell(0,j).value,sheet1.cell(i,j).value,i])
+                            #print([sheet1.cell(0,j).value,sheet1.cell(i,j).value])
                         
                             """
                             auth_value = sheet1.cell(i,begin_column-1).value
